f002d4e47305dccbc8eb3a247f6d00f1877cada1
Pasindu Heshan <p.heshan622@gmail.com>
2025-07-12 22:44:21 +0530

    Assets to assets

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Contact.css'
-import msg_icon from '../../Assets/msg-icon.png'
+import msg_icon from '../../assets/msg-icon.png'
 import mail_icon from '../../assets/mail-icon.png'
 import phone_icon from '../../assets/phone-icon.png'
 import location_icon from '../../assets/location-icon.png'
